f0a5d1537c84f8807522ea4fec4dbcc490a61ae6
chenglin0413 <chenglin0413@gmail.com>
2017-08-18 20:08:40 +0800

    update wb未完成 170818

--- BA102G3/src/serverEndpoint/MyEchoServer.java
+++ BA102G3/src/serverEndpoint/MyEchoServer.java
@@ -1,5 +1,6 @@
 package serverEndpoint;
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import javax.websocket.server.PathParam;
@@ -11,27 +12,38 @@ import javax.websocket.OnError;
 import javax.websocket.OnClose;
 import javax.websocket.CloseReason;
 
-@ServerEndpoint("/MyEchoServer/{myName}/{myRoom}")
+@ServerEndpoint("/MyEchoServer/{storename}/{users}")
 public class MyEchoServer {
-	
-private static final Set<Session> allSessions = Collections.synchronizedSet(new HashSet<Session>());
-	
+	private static final Map<String,Set<Session>>connectedSessionsMap = Collections.synchronizedMap(new HashMap<String,Set<Session>>());
+	private static final String storename=null;
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm"); // 日期格式化
 	@OnOpen
-	public void onOpen(@PathParam("myName") String myName, @PathParam("myRoom") int myRoom, Session userSession) throws IOException {
-		allSessions.add(userSession);
+	public void onOpen(@PathParam("storename") String storename, @PathParam("users") int users, Session userSession) throws IOException {
+		if(connectedSessionsMap.containsKey(storename)){
+			connectedSessionsMap.get(storename).add(userSession);//如果這個加入的商家name已存在map中，則把這位user加入Set<Session>中。
+		}else{
+			Set<Session> sessions=new HashSet<Session>();
+			connectedSessionsMap.put("storename",sessions);//如果這個加入的商家name不在map中，則新增，並把這位user加入Ses<Session>中。
+		}
 		System.out.println(userSession.getId() + ": 已連線");
-		System.out.println(myName + "商家名稱: 已連線");
-		System.out.println(myRoom + ":聊天室_活動中。");
+		System.out.println(storename + "商家名稱: 已連線");
+		System.out.println(users + ":聊天室_活動中。");
 //		userSession.getBasicRemote().sendText("WebSocket 連線成功");
 	}
 
 	
 	@OnMessage
-	public void onMessage(Session userSession, String message) {
-		for (Session session : allSessions) {
-			if (session.isOpen())
+	public void onMessage(Session userSession, String message ) {
+		Iterator<String> it=connectedSessionsMap.keySet().iterator();
+		while(it.hasNext()){
+			String storename=it.next();
+			Set<Session> storeroom=connectedSessionsMap.get(storename);
+			for(Session session:storeroom){
+				if (session.isOpen())
 				session.getAsyncRemote().sendText(message);
+			}
 		}
+		
 		System.out.println("Message received: " + message);
 	}
 	
@@ -42,7 +54,7 @@ private static final Set<Session> allSessions = Collections.synchronizedSet(new 
 	
 	@OnClose
 	public void onClose(Session userSession, CloseReason reason) {
-		allSessions.remove(userSession);
+//		allSessions.remove(userSession);
 		System.out.println(userSession.getId() + ": Disconnected: " + Integer.toString(reason.getCloseCode().getCode()));
 	}
 
